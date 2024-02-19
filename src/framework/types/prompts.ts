a9426ee46e59bb8a2cdf1d5b017292632fb26196
Kasper Welbers <kasperwelbers@gmail.com>
2024-02-19 14:22:29 +0100

    added parameter for whether consentformtable starts in folded state. default false

--- src/framework/types/prompts.ts
+++ src/framework/types/prompts.ts
@@ -1,47 +1,33 @@
-import { isInstanceOf } from '../helpers'
-import { PropsUIRadioItem, Text } from './elements'
+import { isInstanceOf } from "../helpers"
+import { PropsUIRadioItem, Text } from "./elements"
 
 export type PropsUIPrompt =
-<<<<<<< HEAD
-  PropsUIPromptFileInput |
-  PropsUIPromptProgress |
-  PropsUIPromptRadioInput |
-  PropsUIPromptConsentForm |
-  PropsUIPromptConfirm
-
-export function isPropsUIPrompt (arg: any): arg is PropsUIPrompt {
-  return isPropsUIPromptFileInput(arg) ||
-    isPropsUIPromptRadioInput(arg) ||
-    isPropsUIPromptConsentForm(arg)
-=======
   | PropsUIPromptFileInput
   | PropsUIPromptRadioInput
   | PropsUIPromptConsentForm
   | PropsUIPromptConfirm
 
-export function isPropsUIPrompt (arg: any): arg is PropsUIPrompt {
+export function isPropsUIPrompt(arg: any): arg is PropsUIPrompt {
   return isPropsUIPromptFileInput(arg) || isPropsUIPromptRadioInput(arg) || isPropsUIPromptConsentForm(arg)
->>>>>>> 86317f3 (fixing tsstandard stuff)
 }
 
 export interface PropsUIPromptConfirm {
-  __type__: 'PropsUIPromptConfirm'
+  __type__: "PropsUIPromptConfirm"
   text: Text
   ok: Text
   cancel: Text
 }
-export function isPropsUIPromptConfirm (arg: any): arg is PropsUIPromptConfirm {
-  return isInstanceOf<PropsUIPromptConfirm>(arg, 'PropsUIPromptConfirm', ['text', 'ok', 'cancel'])
+export function isPropsUIPromptConfirm(arg: any): arg is PropsUIPromptConfirm {
+  return isInstanceOf<PropsUIPromptConfirm>(arg, "PropsUIPromptConfirm", ["text", "ok", "cancel"])
 }
 
 export interface PropsUIPromptFileInput {
-  __type__: 'PropsUIPromptFileInput'
+  __type__: "PropsUIPromptFileInput"
   description: Text
   extensions: string
 }
 export function isPropsUIPromptFileInput (arg: any): arg is PropsUIPromptFileInput {
   return isInstanceOf<PropsUIPromptFileInput>(arg, 'PropsUIPromptFileInput', ['description', 'extensions'])
-<<<<<<< HEAD
 }
 
 export interface PropsUIPromptProgress {
@@ -52,50 +38,43 @@ export interface PropsUIPromptProgress {
 }
 export function isPropsUIPromptProgress (arg: any): arg is PropsUIPromptProgress {
   return isInstanceOf<PropsUIPromptProgress>(arg, 'PropsUIPromptProgress', ['description', 'message'])
-=======
->>>>>>> 86317f3 (fixing tsstandard stuff)
 }
 
 export interface PropsUIPromptRadioInput {
-  __type__: 'PropsUIPromptRadioInput'
+  __type__: "PropsUIPromptRadioInput"
   title: Text
   description: Text
   items: PropsUIRadioItem[]
 }
-export function isPropsUIPromptRadioInput (arg: any): arg is PropsUIPromptRadioInput {
-  return isInstanceOf<PropsUIPromptRadioInput>(arg, 'PropsUIPromptRadioInput', ['title', 'description', 'items'])
+export function isPropsUIPromptRadioInput(arg: any): arg is PropsUIPromptRadioInput {
+  return isInstanceOf<PropsUIPromptRadioInput>(arg, "PropsUIPromptRadioInput", ["title", "description", "items"])
 }
 export interface PropsUIPromptConsentForm {
-  __type__: 'PropsUIPromptConsentForm'
+  __type__: "PropsUIPromptConsentForm"
   description?: Text
   donateQuestion?: Text
   donateButton?: Text
   tables: PropsUIPromptConsentFormTable[]
   metaTables: PropsUIPromptConsentFormTable[]
 }
-export function isPropsUIPromptConsentForm (arg: any): arg is PropsUIPromptConsentForm {
-  return isInstanceOf<PropsUIPromptConsentForm>(arg, 'PropsUIPromptConsentForm', ['tables', 'metaTables'])
+export function isPropsUIPromptConsentForm(arg: any): arg is PropsUIPromptConsentForm {
+  return isInstanceOf<PropsUIPromptConsentForm>(arg, "PropsUIPromptConsentForm", ["tables", "metaTables"])
 }
 
 export interface PropsUIPromptConsentFormTable {
-  __type__: 'PropsUIPromptConsentFormTable'
+  __type__: "PropsUIPromptConsentFormTable"
   id: string
   title: Text
   description: Text
   data_frame: any
-<<<<<<< HEAD
-}
-export function isPropsUIPromptConsentFormTable (arg: any): arg is PropsUIPromptConsentFormTable {
-  return isInstanceOf<PropsUIPromptConsentFormTable>(arg, 'PropsUIPromptConsentFormTable', ['id', 'title', 'description', 'data_frame'])
-=======
   visualizations: any
+  folded: boolean
 }
-export function isPropsUIPromptConsentFormTable (arg: any): arg is PropsUIPromptConsentFormTable {
-  return isInstanceOf<PropsUIPromptConsentFormTable>(arg, 'PropsUIPromptConsentFormTable', [
-    'id',
-    'title',
-    'description',
-    'data_frame'
+export function isPropsUIPromptConsentFormTable(arg: any): arg is PropsUIPromptConsentFormTable {
+  return isInstanceOf<PropsUIPromptConsentFormTable>(arg, "PropsUIPromptConsentFormTable", [
+    "id",
+    "title",
+    "description",
+    "data_frame",
   ])
->>>>>>> 86317f3 (fixing tsstandard stuff)
 }
