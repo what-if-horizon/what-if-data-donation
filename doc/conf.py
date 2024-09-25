56b7a443bc7fb2b8c989ab5fc17c9bd8dc4b47fc
Max Paulus <paulmaxus@gmail.com>
2024-09-25 15:28:41 +0200

    Sphinx documentation workflow
    * added sphinx autodocs and gh workflow
    
    * added props to docs

---
+++ doc/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../src/framework/processing/py/'))
+
+# Ignore external dependencies
+autodoc_mock_imports = ['pandas', 'numpy']
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Port'
+copyright = '2024, Boeschoten et al.'
+author = 'Boeschoten et al.'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'haiku'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,8 @@
+Port documentation
+==================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   api.md
