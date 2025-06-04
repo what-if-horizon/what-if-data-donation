0d83ec721f63dc0c9ce87f68e909ec0704d870e8
PSUBRodilla <anrola1994@gmail.com>
2025-06-04 11:55:08 +0200

    I push all the changes I made to make the Youtube script working

--- structure_donations/Data_structure_extractors/test_tt.py
+++ structure_donations/Data_structure_extractors/test_tt.py
@@ -9,4 +9,4 @@ sys.path.insert(0, project_root)
 from structure_donations.Data_structure_extractors.TT_get_json_structure import structure_from_json_file
 
 
-structure_from_json_file("structure_donations/Processed_structure_donations/TikTok/Raw/tiktok_takeout_dutch.json")
+#structure_from_json_file("structure_donations/Processed_structure_donations/TikTok/Raw/tiktok_takeout_dutch.json")
