62adab76b70b2a3b5d0fd8b4c8573b1b81d1de24
Wouter van Atteveldt <wouter@vanatteveldt.com>
2025-08-01 15:21:50 +0200

    Simplify args structure

--- pytests/create_scenario.py
+++ pytests/create_scenario.py
@@ -46,79 +46,67 @@ def create_scenario(platform: str, inputfile: Path, tables: list[str] | None):
     return dict(input_file=str(inputfile), platform=platform, expected_output=tables)
 
 
-def render_html(tables, file):
-    print(
-        """<html><head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <style>h1 {padding:.3em}</style>
-  </head><body><div class='container-fluid'>""",
-        file=file,
-    )
-    for name, df in tables.items():
-        print(f"<h1>Table '{name}'</h1>", file=file)
+class Renderers:
+    def json(scenario, file):
+        json.dump(scenario, file, indent=4)
+
+    def html(tables, file):
         print(
-            df.to_html(
-                border=False,
-                index=False,
-                justify="right",
-                classes="thead-dark table-striped table-bordered table-sm table-hover",
-            ),
+            """<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>h1 {padding:.3em}</style>
+    </head><body><div class='container-fluid'>""",
             file=file,
         )
-    print("</div></body></html>", file=file)
+        for table in scenario["expected_output"]:
+            print(f"<h1>Table '{table['id']}'</h1>", file=file)
+            df = pd.DataFrame(data=table["data_frame"]["data"], columns=table["data_frame"]["columns"])
+            print(
+                df.to_html(
+                    border=False,
+                    index=False,
+                    justify="right",
+                    classes="thead-dark table-striped table-bordered table-sm table-hover",
+                ),
+                file=file,
+            )
+        print("</div></body></html>", file=file)
 
-
-def render_md(tables, file):
-    for name, df in tables.items():
-        print(f"\n# Table '{name}'", file=file)
-        print(df.to_markdown(), file=file)
+    def md(tables, file):
+        for table in scenario["expected_output"]:
+            print(f"\n# Table '{table['id']}'\n", file=file)
+            df = pd.DataFrame(data=table["data_frame"]["data"], columns=table["data_frame"]["columns"])
+            print(df.to_markdown(), file=file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("platform", choices=["tiktok", "facebook", "tiktok", "twitter", "youtube"])
     parser.add_argument("inputfile", help="The path or name of the donation file (i.e. .zip or .json)", type=Path)
-    parser.add_argument("tables", nargs="*", help="Specify table ids to extract (default extracts all tables)")
+    parser.add_argument(
+        "outputfile",
+        help="The path or name of the output file (i.e. .json, .html, .md), or '-' to print to stdout",
+    )
+
+    parser.add_argument("tables", nargs="*", help="Specify table ids to extract (default: extracts all tables)")
     parser.add_argument(
         "--list-tables", "-l", action="store_true", help="List only existing table ids and size instead"
     )
-    parser.add_argument("--output", "-o", type=Path, help="Save json into this file instead of printing to stdout")
     parser.add_argument(
-        "--print-tables",
-        "-p",
-        nargs="?",
-        type=Path,
-        const=sys.stderr,
-        help="Print generated tables to this file (or to stderr)",
+        "--format", "-f", choices=["json", "html", "md"], help="Use this format (default: from outfile extension)"
     )
+
     args = parser.parse_args()
 
     logging.basicConfig(format="[%(levelname)-7s:%(name)-15s] %(message)s", level=logging.INFO)
 
     # Make inputfile relative to project root
     inputfile: Path = args.inputfile.resolve().relative_to(Path.cwd())
-    outputfile = sys.stdout if args.output is None else open(args.output, "w")
     if not inputfile.exists():
         raise FileNotFoundError(f"Cannot find input file {inputfile}")
 
     if args.list_tables:
         for id, shape in list_tables(args.platform, inputfile):
             print(f"{id} - {shape[0]} rows x {shape[1]} cols")
-    else:
-        scenario = create_scenario(args.platform, inputfile, tables=args.tables)
-        logging.info(f"Writing output to {outputfile.name}")
-        json.dump(scenario, outputfile, indent=4)
-    print(args.print_tables)
-    if args.print_tables:
-        tables = {
-            x["id"]: pd.DataFrame(data=x["data_frame"]["data"], columns=x["data_frame"]["columns"])
-            for x in scenario["expected_output"]
-        }
-        o = sys.stderr if args.print_tables == sys.stderr else open(args.print_tables, "w")
-        logging.info(f"Printing {len(args.tables)} table{'s' if len(tables) > 1 else ''} to {o.name}")
-        if str(args.print_tables).endswith(".html"):
-            render_html(tables, o)
-        else:
-            render_md(tables, o)
