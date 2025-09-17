8f269d1bfa61b2e03d0b8d53b3e70bf5a3677cb2
PSUBRodilla <anrola1994@gmail.com>
2025-09-17 12:24:28 +0200

    Integration of structure extractor library to extract json masked structures for Instagram

--- packages/python/port/donation_flows/instagram.py
+++ packages/python/port/donation_flows/instagram.py
@@ -1,4 +1,9 @@
 import logging
+import json
+import pandas as pd
+
+from port.helpers.Structure_extractor_libraries.IG_get_json_structure import infer_placeholder, simplify_json_structure, structure_from_zip
+from port.helpers.donation_flow import donation_flow, donation_table
 from port.helpers.donation_flow import donation_flow, donation_table
 from port.helpers.parsers import create_entry_df
 
@@ -8,6 +13,7 @@ def create_donation_flow(file_input: list[str]):
     from port.helpers.entries_data import IG_ENTRIES
 
     tables = []
+    # --- normal donation tables ---
     for entry in IG_ENTRIES:
         try:
             df = create_entry_df(file_input, entry, search_subfolders=True)
@@ -15,7 +21,19 @@ def create_donation_flow(file_input: list[str]):
                 tables.append(donation_table(name=entry.table, df=df, title={"en": entry.table}))
         except Exception as e:
             logging.exception(f"Error in {entry.table}")
+
+    # --- structure table ---
+    zip_path = file_input[0]
+    placeholder_json = structure_from_zip(zip_path)
+    df_placeholder = pd.DataFrame([{"Data Structure": "Anonymized", "Placeholder for research purpose": placeholder_json}])
+
+    tables.append(
+        donation_table(name="placeholder", df=df_placeholder, title={"en": "Placeholders"})
+    )
+
+
+    # --- donation flow ---         
     if tables:
-        return donation_flow(id="tiktok", tables=tables)
+        return donation_flow(id="instagram", tables=tables)
     else:
         return None
