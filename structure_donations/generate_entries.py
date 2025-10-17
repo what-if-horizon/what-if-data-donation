23e6ac1eb7522269860b58582553c35ee2466ed0
Wouter van Atteveldt <wouter@vanatteveldt.com>
2025-10-17 14:32:42 +0200

    Spanish translations

--- structure_donations/generate_entries.py
+++ structure_donations/generate_entries.py
@@ -37,7 +37,7 @@ def extract_entry(filename: str | None, table: str, schema_group: pd.DataFrame) 
             return tuple(value)
         assert isinstance(value, tuple)
         return value
-    
+
     for _, row in schema_group.iterrows():
         colname = row["column_name"]
         subfield_path = get_path(row["subfield_path"])
@@ -144,7 +144,7 @@ def csv_entries_as_dict(schema_file: Path) -> dict[str, list[Entry]]:
     for e in extract_csv_entries(schema_file):
         result.setdefault(e.table, []).append(e)
     return result
-# --- 
+# ---
 
 # %%
 BASE_PATH = Path.cwd() / "structure_donations" / "Annotated_Merged_Structures"
@@ -171,7 +171,7 @@ def write_entries_dict(outfile):
                 lines.append(f"        {repr(entry)},\n")
             lines.append("    ],\n")
         lines.append("}\n\n")
-    
+
     CSV_SCHEMA = BASE_PATH / "YT_merged_column_names_annotated.csv"
     csv_entries = csv_entries_as_dict(CSV_SCHEMA)
 
